99d3cd3afbd1d5081abdb3a38385188f3ba266c5
Shaurya99t <118307828+Shaurya99t@users.noreply.github.com>
2026-04-28 22:28:29 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./fitness.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./fitness_v2.db")
 
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
